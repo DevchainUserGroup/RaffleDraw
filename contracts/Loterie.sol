4e2156664cee8eefc45ab8bad7b2ab7a1afde660
syllabus <labussiere@rocketmail.com>
2017-08-09 21:15:38 +0000

    security checks

--- contracts/Loterie.sol
+++ contracts/Loterie.sol
@@ -1,12 +1,23 @@
 pragma solidity ^0.4.4;
 
 contract Loterie {
+
   event Registration(string someone);
+  event Wins(uint index);
+
   string[] public participants;
-  string public winner;
+  uint  winner;
+  address owner;
+
+	modifier onlyOwner {
+ 		require(msg.sender == owner);
+ 		_;
+	}
+
   function Loterie() {
-    // constructor
+    owner = msg.sender;
   }
+
   function isRegistered(string someone) returns (int) {
     bytes32 hash = sha3(someone);
     for (uint i = 0; i < participants.length; i++) {
@@ -16,21 +27,36 @@ contract Loterie {
     }
     return -1;
   }
-  function registerParticipant(string someone) {
+
+  function registerParticipant(string someone) onlyOwner {
   	int index = isRegistered(someone);
     if (index == -1) {
       participants.push(someone);
       Registration(someone);
     }
   }
+
   function count() returns (uint) {
     return participants.length;
   }
+
   function participant(uint index) returns (string) {
     return participants[index];
   }
-  function drawLoterie() returns (string) {
+
+  function drawLoterie() onlyOwner {
     uint index = uint(block.blockhash(block.number - 1)) % participants.length;
-    winner = participants[index];
+    winner = index;
+    Wins(winner);
+    kill();
   }
+
+  function kill() onlyOwner {
+  	suicide(owner);
+  }
+
+  function getWinner() constant returns (uint) {
+  	return winner;
+  }
+
 }
--- test/loterie.js
+++ test/loterie.js
@@ -1,6 +1,6 @@
 var Loterie = artifacts.require("./Loterie.sol");
 
-contract('Loterie', function(accounts) {
+contract('Loterie with 1 participant', function(accounts) {
   it("should Toto not be participant", function(done) {
     var loterie;
     Loterie.deployed().then(function(instance) {
@@ -25,7 +25,7 @@ contract('Loterie', function(accounts) {
       assert.equal(1,result.valueOf());
       return loterie.participant.call(0);
     }).then(function(result) {
-	    assert.equal('Toto',result.valueOf());
+	    assert.equal('Toto',result);
 	    done();
     });
   });
@@ -44,6 +44,44 @@ contract('Loterie', function(accounts) {
     }).then(function(result) {
       assert.equal(1,result.valueOf());
       done();
+    });
+  });
+ it("should the only participant be the winner", function(done) {
+   var loterie;
+   Loterie.deployed().then(function(instance) {
+     loterie = instance;
+     return instance.registerParticipant('Toto');
+   }).then(function(result) {
+//     loterie.Wins({}, {fromBlock: 0, toBlock: 'latest'}).watch(function(error, logs) {
+//	console.log('==================== AND THE WINNER IS ===========   :   ');
+//	console.log(logs.args);
+//});
+     loterie.drawLoterie();
+   }).then(function(result) {
+     return loterie.getWinner.call();
+   }).then(function(result) {
+     assert.equal(0,result);
+     done();
+   });
+ });
+});
+
+contract('Loterie with 2 participants', function(accounts) {
+  it("should register participants Toto and Titi", function(done) {
+  	var loterie;
+    Loterie.deployed().then(function(instance) {
+    	loterie = instance;
+      return instance.registerParticipant('Toto');
+    }).then(function(result) {
+      return loterie.count.call();
+    }).then(function(result) {
+      assert.equal(1,result.valueOf());
+      return loterie.registerParticipant('Titi');
+    }).then(function(result) {
+      return loterie.count.call();
+    }).then(function(result) {
+      assert.equal(2,result.valueOf());
+	    done();
     });
   });
 });
