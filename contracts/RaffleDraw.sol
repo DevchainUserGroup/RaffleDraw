c5434dc2f1badb1efd5176ebfb72a6746acef49a
cbroillet <cbroillet@yahoo.com>
2017-08-18 21:54:03 +0200

    Test refactoring

--- contracts/RaffleDraw.sol
+++ contracts/RaffleDraw.sol
@@ -3,54 +3,61 @@ pragma solidity ^0.4.11;
 import "./Mortal.sol";
 
 contract RaffleDraw is Mortal {
-    
-    uint private maxParticipants = 10;
-    string[] private participants;
 
-    uint private maxPrizes = 5;
-    string[] private prizes;
+  uint private maxParticipants = 10;
+  string[] private participants;
 
-    string[] private winners;
+  uint private maxPrizes = 5;
+  string[] private prizes;
 
+  string[] private winners;
 
-    function RaffleDraw() public {
-    }
+/* Administrator address */
+  address owner;
 
-    function random(uint modulo) constant returns (uint) {
-        return uint(block.blockhash(block.number - 1)) % modulo;
-    }
+  function RaffleDraw() public {
+    owner = msg.sender;
+  }
 
-    function getRandomWinner() constant returns (string) {
-        string winner = participants[random(participants.length)];
-        addWinner(winner);   
-        return winner;
-    }
+  function random(uint modulo) constant returns (uint) {
+    return uint(block.blockhash(block.number - 1)) % modulo;
+  }
 
-    /* TODO: Make private */
-    function addWinner(string winner) {
-       winners.push(winner);
-    }
+  function getRandomWinner() onlyByAdmin() returns (string) {
+    string winner = participants[random(participants.length)];
+    addWinner(winner);   
+    return winner;
+  }
 
-    function getWinner(uint position) constant returns (string) {
-       return winners[position];
-    }
+  /* TODO: Make private */
+  function addWinner(string winner) onlyByAdmin() {
+    winners.push(winner);
+  }
 
-    function addParticipant(string participant) public {
-       if (participants.length >=  maxParticipants) throw; 
-       participants.push(participant);
-    }
+  function getWinner(uint position) constant returns (string) {
+    return winners[position];
+  }
 
-    function getParticipant(uint position) constant returns (string) {
-       return participants[position];
-    }
-    
-    function addPrize(string prize) public {
-       if (prizes.length >=  maxPrizes) throw;
-       prizes.push(prize);
-    }
+  function addParticipant(string participant) public onlyByAdmin() {
+    if (participants.length >=  maxParticipants) throw; 
+    participants.push(participant);
+  }
 
-    function getPrize(uint position) constant returns (string) {
-       return prizes[position];
-    }
-   
+  function getParticipant(uint position) constant returns (string) {
+    return participants[position];
+  }
+
+  function addPrize(string prize) public onlyByAdmin() {
+    if (prizes.length >=  maxPrizes) throw;
+    prizes.push(prize);
+  }
+
+  function getPrize(uint position) constant returns (string) {
+    return prizes[position];
+  }
+
+  modifier onlyByAdmin() {
+    require(msg.sender == owner);
+    _;
+  }
 }
--- test/RaffleDraw.js
+++ test/RaffleDraw.js
@@ -11,31 +11,34 @@ contract('RaffleDraw', function(accounts) {
 });
 
 contract('RaffleDraw', function(accounts) {
-  var participant = "cbr";
+  var participant1 = "John";
+  var participant2 = "Alice"
+  var raffleDraw;
   it("should return a participant", function() {
     return RaffleDraw.deployed().then(function(instance) {
-      return instance.addParticipant(participant);
-    }).then(function(value){
-      return RaffleDraw.deployed().then(function(instance) {
-        return instance.getParticipant(0);
-      }).then(function(value) {
-        assert.equal(value, participant, "There should be a correct value");
-      });
-    });
+      raffleDraw = instance; 
+      return raffleDraw.addParticipant(participant1);
+    }).then(function(){
+      return raffleDraw.addParticipant(participant2);      
+    }).then(function(){
+      return raffleDraw.getParticipant(1);
+    }).then(function(value) {
+     assert.equal(value, participant2, "There should be a correct value");
+   });     
   });
 });
 
 contract('RaffleDraw', function(accounts) {
   var prize = "ether";
+  var raffleDraw;
   it("should return a prize", function() {
     return RaffleDraw.deployed().then(function(instance) {
-      return instance.addPrize(prize);
-    }).then(function(value){
-      return RaffleDraw.deployed().then(function(instance) {
-        return instance.getPrize(0);
-      }).then(function(value) {
-        assert.equal(value, prize, "There should be a correct value");
-      });
+      raffleDraw = instance;
+      return raffleDraw.addPrize(prize);
+    }).then(function(){
+      return raffleDraw.getPrize(0);
+    }).then(function(value) {
+      assert.equal(value, prize, "There should be a correct value");
     });
   });
 });
@@ -43,49 +46,68 @@ contract('RaffleDraw', function(accounts) {
 contract('RaffleDraw', function(accounts) {
   var prize1 = "ether";
   var prize2 = "bitcoin";
+  var raffleDraw;
   it("should return a second prize", function() {
     return RaffleDraw.deployed().then(function(instance) {
-      return instance.addPrize(prize1);
-    }).then(function(value){
-      return RaffleDraw.deployed().then(function(instance) {
-        return instance.addPrize(prize2);
-      }).then(function(value){
-        return RaffleDraw.deployed().then(function(instance) {
-          return instance.getPrize(1);
-      }).then(function(value) {
-        assert.equal(value, prize2, "There should be a correct value");
-      });
-    });
+      raffleDraw = instance;
+      return raffleDraw.addPrize(prize1);
+    }).then(function(){
+      return raffleDraw.addPrize(prize2);
+    }).then(function(){
+      return raffleDraw.getPrize(1);
+    }).then(function(value) {
+      assert.equal(value, prize2, "There should be a correct value");
     });
   });
 });
 
 contract('RaffleDraw', function(accounts) {
   var participant = "cbr";
+  var raffleDraw;
   it("should return a random winner", function() {
     return RaffleDraw.deployed().then(function(instance) {
-      return instance.addParticipant(participant);
-    }).then(function(value){
-      return RaffleDraw.deployed().then(function(instance) {
-        return instance.getRandomWinner();
-      }).then(function(value) {
-        assert.equal(value, participant, "There should be a correct value");
-      });
+      raffleDraw = instance;
+      return raffleDraw.addParticipant(participant);
+    }).then(function(){
+      return raffleDraw.getRandomWinner();
+    }).then(function(){
+      return raffleDraw.getWinner(0);
+    }).then(function(value) {
+      assert.equal(value, participant, "There should be a correct value");
     });
   });
 });
 
 contract('RaffleDraw', function(accounts) {
   var winner = "cbr";
+  var raffleDraw;
   it("should return a winner", function() {
     return RaffleDraw.deployed().then(function(instance) {
-      return instance.addWinner(winner);
-    }).then(function(value){
-      return RaffleDraw.deployed().then(function(instance) {
-        return instance.getWinner(0);
-      }).then(function(value) {
-        assert.equal(value, winner, "There should be a correct value");
-      });
+      raffleDraw = instance;
+      return raffleDraw.addWinner(winner);
+    }).then(function(){
+      return raffleDraw.getWinner(0);
+    }).then(function(value) {
+      assert.equal(value, winner, "There should be a correct value");
     });
   });
 });
+
+contract('RaffleDraw', function(accounts) {
+  var winner = "cbr";
+  var account_one = accounts[0];
+  // var account_two = accounts[1];
+  var raffleDraw;
+  it("should return a winner", function() {
+    return RaffleDraw.deployed().then(function(instance) {
+      raffleDraw = instance;
+      return raffleDraw.addWinner(winner, {from: account_one});
+    }).then(function(){
+      return raffleDraw.getWinner(0);
+    }).then(function(value) {
+      assert.equal(value, winner, "There should be a correct value");
+    });
+  });
+});
+
+
